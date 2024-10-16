eefe60604c0c4c1aafb3168690b9b78068209b58
Istiak <mhistiak2@gmail.com>
2024-10-16 15:42:54 +0600

    main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import Counter from "./components/Counter.jsx";
 
 const App = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <Counter />
+     
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./reducers/counter.slice";
+
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
+   
   },
 });
 
